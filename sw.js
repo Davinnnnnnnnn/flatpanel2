fecfc8fee46beaec51196b5844b46d4078441e58
Davinnnnnnnnn <sondavin314@gmail.com>
2026-06-06 18:40:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'flatpanel-v1';
+const ASSETS = ['index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
